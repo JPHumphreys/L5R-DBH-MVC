9a209a578a0a8aa807205f337756d0b7f5f1344e
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-25 17:22:42 +0000

    added a model for a deck

---
+++ Models/Deck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L5R_DBH_MVC.Models
+{
+    public class Deck
+    {
+        public string Username { get; set; }
+
+        public string Id { get; set; }
+
+        public string DeckName { get; set; }
+
+        public byte Quantity { get; set; }
+    }
+}
