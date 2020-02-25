fa1f965c552cb3b94a9885517dd9e08c08ac4079
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-25 17:20:54 +0000

    added a model for submitting a vote

---
+++ Models/Vote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L5R_DBH_MVC.Models
+{
+    public class Vote
+    {
+        public string Username { get; set; }//get from ApplicationUser
+
+        public string Id { get; set; }//get from cards
+
+        public string Clan { get; set; }
+
+        public float Rating { get; set; }
+    }
+}
