975b9426e6bcadb228ecadf077371eb263014dfa
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-25 17:15:53 +0000

    added a model for a rating

---
+++ Models/Rating.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L5R_DBH_MVC.Models
+{
+    public class Rating
+    {
+        public string Id { get; set; }
+
+        public float OverallRating { get; set; }
+
+        public float CrabRating { get; set; }
+
+        public float CraneRating { get; set; }
+
+        public float DragonRating { get; set; }
+
+        public float LionRating { get; set; }
+
+        public float PhoenixRating { get; set; }
+
+        public float ScorpionRating { get; set; }
+
+        public float UnicornRating { get; set; }
+
+        public int CrabTotalVotes { get; set; }
+
+        public int CraneTotalVotes { get; set; }
+
+        public int DragonTotalVotes { get; set; }
+
+        public int LionTotalVotes { get; set; }
+
+        public int PhoenixTotalVotes { get; set; }
+
+        public int ScorpionTotalVotes { get; set; }
+
+        public int UnicornTotalVotes { get; set; }
+    }
+}
