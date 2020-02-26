f65011ee95c4f23e43a8843226b0ecbf9d227de0
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-26 16:54:44 +0000

    added a basic viewmodel to display the card and rating in the same view

---
+++ ViewModels/CardAndRatingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using L5R_DBH_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L5R_DBH_MVC.ViewModels
+{
+    public class CardAndRatingViewModel
+    {
+        public Card Card { get; set; }
+        public Rating Rating { get; set; }
+    }
+}
