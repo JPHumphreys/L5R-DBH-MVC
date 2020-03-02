35d8d17b5a923e75265509c8881f386e949a184f
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-03-02 10:35:57 +0000

    created a controller for the vote model

--- Models/Vote.cs
+++ Models/Vote.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace L5R_DBH_MVC.Models
 {
@@ -11,8 +12,11 @@ namespace L5R_DBH_MVC.Models
 
         public string Id { get; set; }//get from cards
 
+        [StringLength(8)]//scorpion longest string
+        [MinLength(4)]//crab,lion
         public string Clan { get; set; }
 
+        [Range(1.0,10.0)]
         public float Rating { get; set; }
     }
 }
