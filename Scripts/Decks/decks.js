40823f7b7a1abc14305f3d7277602b1f26d4db94
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-03-14 16:23:28 +0000

    created the outline of the function interaction for creating decks

--- Scripts/Decks/decks.js
+++ Scripts/Decks/decks.js
@@ -3,4 +3,35 @@
 
 function BuildDeck(deckName, primary, secondary, role, element) {
 
+
+
+    CreatePrimary(primary);
+    CreateSecondary(secondary);
+    CreateRole(role);
+    CreateElement(element);
+    CreateDeckButtons();
+
+
+
+}
+
+function CreateDeckButtons() {
+
+}
+
+
+function CreatePrimary(primary) {
+
+}
+
+function CreateSecondary(secondary) {
+
+}
+
+function CreateRole(role) {
+
+}
+
+function CreateElement(element) {
+
 }
