f49bccdb3254bf9856fecf165d9114d0c9a8702d
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-03-18 11:40:33 +0000

    created a deckname attribute for the deck

--- Scripts/Decks/decks.js
+++ Scripts/Decks/decks.js
@@ -3,15 +3,19 @@ const deckRenderLocation = document.getElementById("deck-render-location");
 
 function BuildDeck(deckName, primary, secondary, role, element) {
 
-    let deck;
+    let deck = document.createElement("div");
+    deck.classList.add("col-sm-6", "col-md-4", "col-lg-3", "deck");
     //add the deckname here
-
+    let name = document.createElement("h3");
+    name.classList.add("deckname");
+    name.innerText = deckName;
 
     CreatePrimary(primary);
     CreateSecondary(secondary);
     CreateRole(role);
     CreateElement(element);
-    CreateDeckButtons();
+
+    deck.appendChild(name, CreateDeckButtons());
 
 
     deckRenderLocation.appendChild(deck);
@@ -43,6 +47,8 @@ function CreateDeckButtons() {
     remove.addEventListener("click", function(){
         DeleteDeck(this);
     });
+
+    buttonRow.appendChild(copy, remove);
 
     return buttonRow;
 }
