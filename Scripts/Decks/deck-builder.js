d503d6eabdd52aad46a6bb03144ba8df5bf83a9d
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-03-18 14:58:51 +0000

    moved code from the decks.js file to the new deck-builder.js file

--- Scripts/Decks/deck-builder.js
+++ Scripts/Decks/deck-builder.js
@@ -1,1 +1,76 @@
 ﻿
+function CreatePrimary(name) {
+
+    let primary = document.createElement("div");
+    primary.classList.add("col-8", "primary");
+
+    let text = document.createElement("h4");
+    text.innerText = name;
+
+    primary.appendChild(text);
+
+    return primary;
+
+}
+
+function CreateRole(name) {
+
+    let role = document.createElement("div");
+    role.classList.add("col-3", "role");
+
+    let image = document.createElement("img");
+    image.setAttribute("src", "/Content/Media/" + name + "_colour.jpg");
+    image.setAttribute("alt", name);
+
+    role.appendChild(image);
+
+    return role;
+}
+
+
+function CreateTopContainer(primaryName, roleName) {
+
+    let container = document.createElement("div");
+    container.classList.add("row", "deck-top-container");
+
+    container.appendChild(CreatePrimary(primaryName), CreateRole(roleName));
+
+    return container;
+}
+
+function CreateBottomContainer(secondaryName, elementName) {
+
+}
+
+function CreateSecondary(name) {
+
+}
+
+
+function CreateElement(name) {
+
+}
+
+function CreateDeckButtons() {
+
+    let buttonRow = document.createElement("div");
+    buttonRow.classList.add("row", "btn-group", "button-row");
+
+    let copy = document.createElement("button");
+    copy.classList.add("btn", "btn-md", "btn-dark", "copy-button");
+    copy.innerText = "Copy";
+    copy.addEventListener("click", function () {
+        CopyDeck(this);
+    });
+
+    let remove = document.createElement("button");
+    remove.classList.add("btn", "btn-md", "btn-dark", "delete-button");
+    remove.innerText = "Delete";
+    remove.addEventListener("click", function () {
+        DeleteDeck(this);
+    });
+
+    buttonRow.appendChild(copy, remove);
+
+    return buttonRow;
+}
